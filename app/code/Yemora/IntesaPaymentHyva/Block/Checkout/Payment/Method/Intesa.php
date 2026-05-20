d4cd103fe4981f3d10d1ddb3228e79640782cb87
Mihailo Krgovic <mihailokrgovic@hotmail.com>
2026-05-20 10:40:15 +0200

    Added Hyva adapter

---
+++ app/code/Yemora/IntesaPaymentHyva/Block/Checkout/Payment/Method/Intesa.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yemora\IntesaPaymentHyva\Block\Checkout\Payment\Method;
+
+use Magento\Framework\View\Element\Template;
+use Yemora\IntesaPayment\Model\Ui\ConfigProvider;
+
+class Intesa extends Template
+{
+    public function __construct(
+        Template\Context $context,
+        private readonly ConfigProvider $configProvider,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    public function getDescription(): string
+    {
+        $config = $this->configProvider->getConfig();
+
+        return (string) ($config['payment'][ConfigProvider::CODE]['description'] ?? '');
+    }
+}
